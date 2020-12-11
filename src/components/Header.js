e2f13c87b252a54112ff03c07e6b198953eee7f4
Ahmed <asaad.memon@gmail.com>
2020-12-11 14:45:02 +0300

    Added Date

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export const Header = () => {
     return (
         <h1>
             Expense Tracker
+            <div>
+                {Date()}
+            </div>
         </h1>
     )
 }
